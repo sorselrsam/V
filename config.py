8393caa8400c0a46b9c7b1d82748c263a5aba2f7
SOURCE SPIDER <117318674+SOURCE-SPIDER@users.noreply.github.com>
2023-04-15 10:05:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/ba5e50feaff3c2bbde984.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/ba5e50feaff3c2bbde984.jpg")
+PING_IMG = getenv("PING_IMG", "https://graph.org/file/9681d7086cc9c32b85d5a.jpg")
+START_IMG = getenv("START_IMG", "https://graph.org/file/c1c0f856a869169722289.jpg")
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EE_20")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
 
 
-FAILED = "https://telegra.ph/file/ba5e50feaff3c2bbde984.jpg"
+FAILED = "https://graph.org/file/9681d7086cc9c32b85d5a.jpg"
